868d023056d1f4895fe8a2196339e5c714b40159
TomLogg <128543069+TomLogg@users.noreply.github.com>
2025-04-22 15:54:38 -0400

    final

--- script.js
+++ script.js
@@ -13,4 +13,29 @@ function addTask() {
     li.appendChild(span);
   }
   inputBox.value = "";
+  saveData();
 }
+// if clicked target element is li it will check and uncheck item
+//if clicked target element is span it will remove item
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+// saves list container innerhtml in localstorage
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+//displays saves data when opened
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
